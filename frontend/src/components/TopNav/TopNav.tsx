fba703990e3f346823928d1cd0721248b7c50a99
tenal <tenalbourchier@gmail.com>
2023-08-07 12:18:44 -0400

    get prettier/eslint formatting files correctly, add additional paths, minor updates to nav file

--- frontend/src/components/TopNav/TopNav.tsx
+++ frontend/src/components/TopNav/TopNav.tsx
@@ -1,62 +1,68 @@
 import React, { useState } from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import MenuIcon from '@mui/icons-material/Menu';
-import AdbIcon from '@mui/icons-material/Adb';
-
-import HideOnScroll from './HideOnScroll'
-
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import Container from '@mui/material/Container'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import Tooltip from '@mui/material/Tooltip'
+import MenuItem from '@mui/material/MenuItem'
+import MenuIcon from '@mui/icons-material/Menu'
+import AdbIcon from '@mui/icons-material/Adb'
+import { Link } from 'react-router-dom'
 
 function TopNav() {
-    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
-    const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
+    const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
 
-    const pages = ['About', 'FAQs', 'Reviews'];
-    const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+    const pages = ['About', 'FAQs', 'Reviews']
+    const settings = ['Profile', 'Account', 'Dashboard', 'Logout']
 
     const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElNav(event.currentTarget);
-    };
+        setAnchorElNav(event.currentTarget)
+    }
+
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElUser(event.currentTarget);
-    };
+        setAnchorElUser(event.currentTarget)
+    }
 
     const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+        setAnchorElNav(null)
+    }
 
     const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+        setAnchorElUser(null)
+    }
 
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters className="topNav">
                     {/* left | med screens: logo pic & text */}
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{ display: { xs: 'none', md: 'flex' } }}
-                        className="logoText"
-                    >
-                        Boutique Boarding
-                    </Typography>
+                    <AdbIcon
+                        sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
+                    />
+                    <Link to="/">
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            sx={{ display: { xs: 'none', md: 'flex' } }}
+                            className="logoText"
+                        >
+                            Boutique Boarding
+                        </Typography>
+                    </Link>
 
                     {/* left | small screens: ham menu + menu items */}
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'flex', md: 'none' },
+                        }}
+                    >
                         <IconButton
                             size="large"
                             aria-label="account of current user"
@@ -71,96 +77,126 @@ function TopNav() {
                             id="menu-appbar"
                             anchorEl={anchorElNav}
                             anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
+                                vertical: 'bottom',
+                                horizontal: 'left',
                             }}
                             keepMounted
                             transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
+                                vertical: 'top',
+                                horizontal: 'left',
                             }}
                             open={Boolean(anchorElNav)}
                             onClose={handleCloseNavMenu}
                             sx={{
-                            display: { xs: 'block', md: 'none' },
+                                display: { xs: 'block', md: 'none' },
                             }}
                         >
                             {pages.map((page) => (
-                            <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                <Typography textAlign="center">{page}</Typography>
-                            </MenuItem>
+                                <MenuItem
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Link
+                                        key={page}
+                                        to={`/${page.toLowerCase()}`}
+                                        onClick={handleCloseNavMenu}
+                                        style={{
+                                            display: 'block',
+                                            textDecoration: 'none',
+                                            textAlign: 'center',
+                                        }}
+                                    >
+                                        <Typography>{page}</Typography>
+                                    </Link>
+                                </MenuItem>
                             ))}
                         </Menu>
                     </Box>
 
                     {/* center | small screens: logo pic & text */}
-                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <AdbIcon
+                        sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}
+                    />
                     <Typography
-                    variant="h5"
-                    noWrap
-                    component="a"
-                    href=""
-                    sx={{
-                        mr: 2,
-                        display: { xs: 'flex', md: 'none' },
-                        flexGrow: 1,
-                        fontFamily: 'monospace',
-                        fontWeight: 700,
-                        letterSpacing: '.3rem',
-                        color: 'inherit',
-                        textDecoration: 'none',
-                    }}
+                        variant="h6"
+                        noWrap
+                        sx={{
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                        }}
+                        className="logoText"
                     >
-                        Boutique Boarding
+                        <Link to="/"> Boutique Boarding </Link>
                     </Typography>
 
                     {/* right | med screens: menu items */}
-                    <Box mr={2} sx={{ flexGrow: 1, justifyContent: 'flex-end', display: { xs: 'none', md: 'flex' } }}>
-                    {pages.map((page) => (
-                        <Button
-                        key={page}
-                        onClick={handleCloseNavMenu}
-                        sx={{ my: 2, color: 'white', display: 'block' }}
-                        >
-                        {page}
-                        </Button>
-                    ))}
+                    <Box
+                        mr={2}
+                        sx={{
+                            flexGrow: 1,
+                            justifyContent: 'flex-end',
+                            display: { xs: 'none', md: 'flex' },
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <Button>
+                                <Link
+                                    key={page}
+                                    to={`/${page.toLowerCase()}`}
+                                    onClick={handleCloseNavMenu}
+                                    className="links"
+                                >
+                                    <Typography>{page}</Typography>
+                                </Link>
+                            </Button>
+                        ))}
                     </Box>
 
                     {/* right | all screens: profile pic + options */}
                     <Box sx={{ flexGrow: 0 }}>
-                    <Tooltip title="Open settings">
-                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                        </IconButton>
-                    </Tooltip>
-                    <Menu
-                        sx={{ mt: '45px' }}
-                        id="menu-appbar"
-                        anchorEl={anchorElUser}
-                        anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                        }}
-                        open={Boolean(anchorElUser)}
-                        onClose={handleCloseUserMenu}
-                    >
-                        {settings.map((setting) => (
-                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                            <Typography textAlign="center">{setting}</Typography>
-                        </MenuItem>
-                        ))}
-                    </Menu>
+                        <Tooltip title="Open settings">
+                            <IconButton
+                                onClick={handleOpenUserMenu}
+                                sx={{ p: 0 }}
+                            >
+                                <Avatar
+                                    alt="Remy Sharp"
+                                    src="/static/images/avatar/2.jpg"
+                                />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem
+                                    key={setting}
+                                    onClick={handleCloseUserMenu}
+                                >
+                                    <Typography textAlign="center">
+                                        {setting}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
                     </Box>
                 </Toolbar>
             </Container>
         </AppBar>
-        );
+    )
 }
 
 export default TopNav
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { RecoilRoot } from "recoil";
-import { CookiesProvider } from "react-cookie";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
+import { CookiesProvider } from 'react-cookie'
 
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./styling/mui/theme";
-import "./styling/css/App.scss";
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './styling/mui/theme'
+import './styling/css/App.scss'
 
-import Home from "./views/Home/Home";
+import Home from './views/Home/Home'
 
 function App() {
     return (
@@ -17,18 +17,21 @@ function App() {
                 <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<Home />} />
+                        <Route path="/faqs" element={<Home />} />
+                        <Route path="/reviews" element={<Home />} />
                     </Routes>
                 </BrowserRouter>
             </ThemeProvider>
         </CookiesProvider>
-    );
+    )
 }
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         <RecoilRoot>
             <App />
         </RecoilRoot>
     </React.StrictMode>
-);
+)
