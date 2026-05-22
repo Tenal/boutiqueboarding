48e3a37c155d5efa876bdd4ad0b734e9848d4291
tenal <tenalbourchier@gmail.com>
2026-05-22 11:16:13 -0400

    feat: add page transitions and scroll reveal utility
    Wraps routes in AnimatePresence for cross-page fade transitions and
    extracts a useScrollReveal hook for staggered on-scroll animations.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 import { CookiesProvider } from 'react-cookie'
+import { AnimatePresence } from 'framer-motion'
 
 import { ThemeProvider } from '@mui/material/styles'
 import theme from './styling/mui/theme'
@@ -13,17 +14,27 @@ import About from './views/About/About'
 import Faqs from './views/Faqs/Faqs'
 import Reviews from './views/Reviews/Reviews'
 
+function AnimatedRoutes() {
+    const location = useLocation()
+
+    return (
+        <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo({ top: 0, behavior: 'instant' })}>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/faqs" element={<Faqs />} />
+                <Route path="/reviews" element={<Reviews />} />
+            </Routes>
+        </AnimatePresence>
+    )
+}
+
 function App() {
     return (
         <CookiesProvider>
             <ThemeProvider theme={theme}>
                 <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/faqs" element={<Faqs />} />
-                        <Route path="/reviews" element={<Reviews />} />
-                    </Routes>
+                    <AnimatedRoutes />
                 </BrowserRouter>
             </ThemeProvider>
         </CookiesProvider>
